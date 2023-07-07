8ea6fbf4b8ea112a865507ab8edc5ea6c176f25c
salbyul <kpast96@gmail.com>
2023-07-07 20:14:13 +0900

    feat(baseball) : '숫자를 입력해주세요: ' 출력

---
+++ src/main/java/baseball/InputView.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class InputView {
+    public void printInputNumber() {
+        System.out.print("숫자를 입력해주세요: ");
+    }
+}
---
+++ src/test/java/baseball/InputViewTest.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class InputViewTest {
+
+    private static ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    void setUp() {
+        outputStream = new ByteArrayOutputStream(); // outputStream 생성
+        System.setOut(new PrintStream(outputStream)); // 생성한 outputStream으로 설정
+    }
+
+    @Test
+    @DisplayName("숫자 입력 출력")
+    void printInputNumber() {
+        InputView inputView = new InputView();
+        inputView.printInputNumber();
+        Assertions.assertThat("숫자를 입력해주세요: ").isEqualTo(outputStream.toString());
+    }
+}
